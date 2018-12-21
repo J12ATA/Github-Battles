5c871b2401f9791e996623ec7ec65aecf5410a4b
Jose Figueroa <Josefigueroalv@protonmail.com>
2018-12-20 20:44:56 -0800

    first commit

---
+++ app/components/Loading.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const styles = {
+  content: {
+    textAlign: 'center',
+    fontSize: '35px'
+  }
+};
+
+class Loading extends React.Component {
+  state = {
+    text: this.props.text
+  }
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    speed: PropTypes.number.isRequired
+  }
+  
+  static defaultProps = {
+    text: 'Loading',
+    speed: 300
+  }
+  componentDidMount() {
+    const {text, speed} = this.props,
+    stopper = text + '...';
+    this.interval = window.setInterval(() => {
+      this.state.text === stopper
+        ? this.setState(() => ({text: this.props.text}))
+        : this.setState((prevState) => ({text: prevState.text + '.'}))
+    }, speed)
+  }
+  componentWillUnmount() {
+    console.log('CLEAR THE INTERVAL!')
+    window.clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <p style={styles.content}>
+        {this.state.text}
+      </p>
+    )
+  }
+}
+
+export default Loading;
---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+
+
+// State
+// Life-cycle events
+// UI
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
